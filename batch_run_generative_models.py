8c40a81dc5de9736517771615b3e4023ade7e705
nicktfranklin <nthompsonsonfranklin@gmail.com>
2020-02-26 09:46:32 -0500

    updated for revisions

--- batch_run_generative_models.py
+++ batch_run_generative_models.py
@@ -341,6 +341,50 @@ def batch_exp_4_goals(seed=0, n_sims=1000, alpha_mu=0.0, alpha_scale=1.0, goal_p
     results_fl = None
 
 
+def merge_dfs(tag, file_path='./data/'):
+    model_list = 'joint indep meta flat q nctxq kal'.split()
+
+    # # load experiment 2 goals
+    print "Loading Experiment 1"
+    sims_exp_2 = []
+    for m in model_list:
+        df = pd.read_pickle(file_path + 'exp_2_goals_batch_of_sims_{}{}.pkl'.format(m, tag))
+        df.drop(['Start Location', 'End Location', 'Goal Locations', 'Action Map', 'Walls'], axis=1, inplace=True)
+        df = df[df['In Goal']]
+        sims_exp_2.append(df)
+    df = None
+    sims_exp_2 = pd.concat(sims_exp_2, sort=False)
+    sims_exp_2.to_pickle(file_path + 'exp_2_goals_batch_of_sims{}.pkl'.format(tag))
+    sims_exp_2 = None
+
+    # load experiment 4 goals
+    print "Loading Experiment 2"
+    sims_exp_4 = []
+    for m in model_list:
+        df = pd.read_pickle(file_path + 'exp_4_goals_batch_of_sims_{}{}.pkl'.format(m, tag))
+        df.drop(['Start Location', 'End Location', 'Goal Locations', 'Action Map', 'Walls'], axis=1, inplace=True)
+        df = df[df['In Goal']]
+        sims_exp_4.append(df)
+    df = None
+    sims_exp_4 = pd.concat(sims_exp_4, sort=False)
+    sims_exp_4.to_pickle(file_path + 'exp_4_goals_batch_of_sims{}.pkl'.format(tag))
+    sims_exp_4 = None
+
+
+    # load experiment 3 goals
+    print "Loading Experiment 3"
+    sims_exp_3 = []
+    for m in model_list:
+        df = pd.read_pickle(file_path + 'exp_3_goals_batch_of_sims_{}{}.pkl'.format(m, tag))
+        df.drop(['Start Location', 'End Location', 'Goal Locations', 'Action Map', 'Walls'], axis=1, inplace=True)
+        df = df[df['In Goal']]
+        sims_exp_3.append(df)
+    df = None
+    sims_exp_3 = pd.concat(sims_exp_3, sort=False)
+    sims_exp_3.to_pickle(file_path + 'exp_3_goals_batch_of_sims{}.pkl'.format(tag))
+    sims_exp_3 = None
+    print "Done!"
+
 if __name__ == "__main__":
     kwargs = dict(
         n_sims              = 2500,
@@ -357,3 +401,7 @@ if __name__ == "__main__":
     batch_exp_4_goals(**kwargs)
     batch_exp_2_goals(**kwargs)
     batch_exp_3_goals(**kwargs)
+
+    ## combine all of the dataframes into one per experiment for simplicity
+    tag = '_update_all_trials__gp=1e-10_prune=500_mu=-0.5_scale=1.0_invtemp_mu=2.0_invtemp_scale=0.5'
+    merge_dfs(tag)
